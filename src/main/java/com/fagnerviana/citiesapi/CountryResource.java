0f3bf65e325b6f1807dbf00b383cabf26a62693e
fagnerviana <fagsantiago@gmail.com>
2022-01-03 21:11:06 -0300

    GetMapping atualizada busca ID

--- src/main/java/com/fagnerviana/citiesapi/CountryResource.java
+++ src/main/java/com/fagnerviana/citiesapi/CountryResource.java
@@ -2,11 +2,16 @@ package com.fagnerviana.citiesapi;
 
 import com.fagnerviana.citiesapi.countries.Country;
 import com.fagnerviana.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -18,9 +23,22 @@ public class CountryResource {
      public CountryResource(CountryRepository repository){
          this.repository = repository;
      }
-
+//Chama a lista de paises do banco de dados
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
+    //Chama a lista de paise por ID
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        // Retorna o resultado da pesquisa
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
